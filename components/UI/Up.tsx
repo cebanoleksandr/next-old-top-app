cfbdb81db8a072578bbb7c67c8851c5a1b2682c4
Oleksandr Cheban <cebanoleksandr@gmail.com>
2025-12-23 15:56:25 +0200

    Add ButtonIcon

--- components/UI/Up.tsx
+++ components/UI/Up.tsx
@@ -3,6 +3,7 @@ import { ChevronUpIcon } from "@heroicons/react/16/solid";
 import { useAnimation } from "framer-motion";
 import { motion } from "framer-motion";
 import { useEffect } from "react";
+import ButtonIcon from "./ButtonIcon";
 
 const Up = () => {
   const controls = useAnimation();
@@ -29,7 +30,7 @@ const Up = () => {
       animate={controls}
       initial={{ opacity: 0, pointerEvents: 'none' }}
     >
-      <ChevronUpIcon className="size-7 text-white" />
+      <ButtonIcon icon="up" onClick={scrollToTop} />
     </motion.div>
   );
 };
