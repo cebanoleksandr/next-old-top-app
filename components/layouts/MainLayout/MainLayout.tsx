46158097dfce6fc66cdd227d3dfbccdf9215c513
Oleksandr Cheban <cebanoleksandr@gmail.com>
2025-12-26 15:25:41 +0200

    Add SkipLink

--- components/layouts/MainLayout/MainLayout.tsx
+++ components/layouts/MainLayout/MainLayout.tsx
@@ -1,22 +1,58 @@
-import { FC, ReactNode } from "react";
+import { FC, KeyboardEvent, ReactNode, useRef, useState } from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 import Sidebar from "../Sidebar";
 import "./MainLayout.css";
 import Up from "@/components/UI/Up";
+import cn from "classnames";
 
 interface IProps {
   children: ReactNode;
 }
 
 const MainLayout: FC<IProps> = ({ children }) => {
+  const [isSkipLinkVisible, setIsSkipLinkVisible] = useState(false);
+
+  const bodyRef = useRef<HTMLDivElement>(null);
+
+  const skipContentAction = (e: KeyboardEvent) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      e.preventDefault();
+      bodyRef.current?.focus();               
+    }
+
+    setIsSkipLinkVisible(false);
+  };
+
   return (
     <div className="wrapper">
+      <a
+        href="" 
+        tabIndex={1} 
+        className={cn("block fixed left-25 top-0 overflow-hidden h-0 bg-[var(--primary)] text-white", {
+          "h-auto": isSkipLinkVisible
+        })}
+        onFocus={() => setIsSkipLinkVisible(true)}
+        onBlur={() => setIsSkipLinkVisible(false)}
+        onKeyDown={skipContentAction} 
+      >
+        Сразу к содержанию
+      </a>
+
       <div className="md:hidden">
         <Header className="header" />
       </div>
+
       <Sidebar className="sidebar" />
-      <main className="main">{children}</main>
+
+      <main
+        className="main"
+        ref={bodyRef}
+        tabIndex={0}
+      >
+        {children}
+      </main>
+        
       <Footer className="footer" />
       <Up />
     </div>
